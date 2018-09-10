e3dd3714f951af4d30ca1c22a76a90732302589e
gcuenya <42759901+gcuenya@users.noreply.github.com>
2018-09-10 03:54:00 -0300

    Update_serv_deg_dmark.py
    Revise el código y en general me parece bien. Al pie del código deje anotaciones de modificaciones que me parece que sera bueno agregar y un par de dudas. Igualmente, lo sigo trabajando.

--- serv_deg_dmark.py
+++ serv_deg_dmark.py
@@ -11,19 +11,20 @@ el mismo, y tendría como resultado su dirección IP. var = ipaddr_hub'''
 # Conexión al concentrador
 # net_connect = ConnectHandler(device_type='cisco_ios_telnet', ip='192.168.5.142', username='ign', password='auto')
 
-net_connect = ConnectHandler(
-    device_type='cisco_ios_telnet', ip=hub_device.ipaddr_hub,
-    username=user_passw.USER, password=user_passw.passw)
+net_connect = ConnectHandler(device_type='cisco_ios_telnet', ip=hub_device.ipaddr_hub,username=user_passw.USER, password=user_passw.passw)
 
 # En el concentrador ejecutar "show ip nhrp ipaddr_tun" y almacenar en la variable ipaddr_nbma
 ipaddr_nbma = net_connect.send_command('show ip nhrp ' + ipaddr_tun)
 ipaddr_nbma = ipadrr_nbma[17:30].replace(' ', '')
 
 # Pérdidas fuera del túnel (por ende también dentro)
+#Agrego pin por dentro del tunel para el caso que de bien por fuera
 # Analizar el ping
-ping = net_connect.send_command(
-    'ping ' + ipaddr_nbma + ' repeat 100 timeout 1')
-print(ping)
+ping_out = net_connect.send_command('ping ' + ipaddr_nbma + ' repeat 100 timeout 1')
+print(ping_out)
+net_connect.disconnect()
+ping_in = net_connect.send_command('ping ' + '192.168.5.142' + ' repeat 100 timeout 1')
+print(ping_in)
 net_connect.disconnect()
 
 # Generar variable de modelo del equipo
@@ -38,9 +39,7 @@ net_connect.disconnect()
 
 # Verificar contadores y negociación de la interfaz conectada al proveedor
 if dev_vers in 'C8':
-    net_connect = ConnectHandler(
-        device_type='cisco_ios_telnet', ip='192.168.5.142',
-        username='ign', password='auto')
+    net_connect = ConnectHandler(device_type='cisco_ios_telnet', ip='192.168.5.142',username='ign', password='auto')
     inet_counters = net_connect.send_command('show interfaces fastethernet4 | i duplex|CRC|collision|Last clearing|Description')
     net_connect.disconnect()
 else:
@@ -50,18 +49,14 @@ else:
 
 print(inet_counters)
 
-# Verificar contadores y negociación de la interfaz conectada al proveedor
+# Verificar contadores y negociación de la interfaz conectada al cliente
 if dev_vers in 'C8':
     net_connect = ConnectHandler(device_type='cisco_ios_telnet', ip='192.168.5.142', username='ign', password='auto')
-    inet_counters = net_connect.send_command(
-        'show interfaces fastethernet1 | i duplex|CRC|collision|Last clearing|Description')
+    inet_counters = net_connect.send_command('show interfaces fastethernet1 | i duplex|CRC|collision|Last clearing|Description')
     net_connect.disconnect()
 else:
-    net_connect = ConnectHandler(
-        device_type='cisco_ios_telnet', ip='192.168.5.142',
-        username='ign', password='auto')
-    inet_counters = net_connect.send_command(
-        'show interfaces fastethernet0/1 | i duplex|CRC|collision|Last clearing|Description')
+    net_connect = ConnectHandler(device_type='cisco_ios_telnet', ip='192.168.5.142',username='ign', password='auto')
+    inet_counters = net_connect.send_command('show interfaces fastethernet0/1 | i duplex|CRC|collision|Last clearing|Description')
     net_connect.disconnect()
 
 print(inet_counters)
@@ -75,7 +70,17 @@ print(inet_counters)
 end_time = datetime.now()
 total_time = end_time - start_time
 print(total_time)
-# $$$No me gusta$$$#
-# show running-configuration interface tunnel_x | i mtu
-# ip mtu 1400
+
+#Agrego pin por dentro del tunel para el caso que de bien por fuera
+#Falta:
+#cuando se verificar contadores y negociación de la interfaz conectada al proveedor tambien se deberia vericar para el lado del cliente.
+#Ping sin y con mtu 
+#trace route desde el concentrador al PE
+#Dudas del codigo:
+#if dev_vers in 'C8':
+#show interfaces fastethernet4
+#show interfaces fastethernet0/0
+
+
+
 
